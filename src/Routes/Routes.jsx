e64bc89054b0205febb1df530ff0fa50d3e69ea1
fahadbgnr <aminulislamfahad1@gmail.com>
2025-04-16 21:29:48 +0600

    navbar and footar install done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    createBrowserRouter,
+  } from "react-router";
+import Root from '../pages/Root/Root';
+import ErrorPages from '../pages/ErrorPages/ErrorPages';
+import Home from '../pages/Home/Home';
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Root,
+      errorElement: <ErrorPages></ErrorPages>,
+      children:[
+        {
+            index: true,
+            path:"/",
+            Component: Home
+        }
+      ]
+    },
+  ]);
